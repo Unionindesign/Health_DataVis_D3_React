1aa3f60a373da132397ea43d7f5a4e91beae0c6f
iMac <imac@SM-iMac.local>
2020-08-03 21:46:53 -0600

    Scatterplot is working. Need to add Axes, refine chart sizing as some dots are out of viewport. Close to complete

--- src/App.js
+++ src/App.js
@@ -1,42 +1,98 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { csv } from "d3";
-// import { range } from "d3-array";
-// import { scaleOrdinal } from "d3-scale";
+import _ from "lodash";
 
 //components
 import ScatterPlot from "./components/scatterplot";
 
 function App() {
+  //intitial application state
   const [loadingData, setLoadingData] = useState(false);
   const [healthData, setHealthData] = useState([]);
-
+  const [chartWidth, setChartWidth] = useState(0);
+  const [chartHeight, setChartHeight] = useState(0);
+  //update-able x and y dimensions
+  const [xDimensionKey, setXDimensionKey] = useState("obesity");
+  const [xDimensionData, setXDimensionData] = useState([]);
+  const [yDimensionKey, setYDimensionKey] = useState("income");
+  const [yDimensionData, setYDimensionData] = useState([]);
+  //ref to chart container div node
+  const chartContainerDiv = useRef(null);
   useEffect(() => {
+    //when the app loads, set the loading state to true
     setLoadingData(true);
+    //load the csv data with d3's csv method
     csv("./data/health_data.csv").then((data) => {
       console.log("Health Data: ", data);
+      //load the data to the application state with the useState hook
       setHealthData(data);
+      //calculate the size of the div when it loads, and pass this to chart component
+      setChartWidth(chartContainerDiv.current.getBoundingClientRect().width);
+      setChartHeight(chartContainerDiv.current.getBoundingClientRect().height);
+      //set loading state to false
       setLoadingData(false);
     });
   }, []);
+
+  const handleXDimensionSelect = (event) => {
+    console.log("Select X: ", event.target.value);
+    setXDimensionKey(event.target.value);
+    setXDimensionData(
+      healthData.map((d) => {
+        return d[event.target.value];
+      })
+    );
+  };
+  const handleYDimensionSelect = (event) => {
+    console.log("Select Y: ", event.target.value);
+    setYDimensionKey(event.target.value);
+    setYDimensionData(healthData.map((d) => d[event.target.value]));
+  };
 
   return (
     <div className="App">
       <h1>Health Risk Factors</h1>
       <div className="chart-container">
-        <div className="left-chart-main" id="plot">
+        <div className="left-chart-main" ref={chartContainerDiv}>
           <ScatterPlot
             data={healthData}
-            xDimension={(d) => d.number1}
-            yDimension={(d) => d.number2}
+            width={chartWidth}
+            height={chartHeight}
+            xDimensionKey={xDimensionKey}
+            yDimensionKey={yDimensionKey}
+            xDimensionData={
+              _.isEmpty(xDimensionData)
+                ? healthData.map((d) => d.obesity)
+                : xDimensionData
+            }
+            yDimensionData={
+              _.isEmpty(yDimensionData)
+                ? healthData.map((d) => d.income)
+                : yDimensionData
+            }
           />
         </div>
         <div className="right-chart-filters">
           <h5>Filters:</h5>
           <form>
-            <label>Options</label>
-            <select>
-              <option>option 1</option>
+            <label>Health Indicators:</label>
+            <select onChange={handleXDimensionSelect}>
+              <option value="age">Age</option>
+              <option value="obesity" default>
+                Obesity
+              </option>
+              <option value="smokes">Smokes</option>
+            </select>
+          </form>
+          <form>
+            <label>Environmental Factors:</label>
+            <select onChange={handleYDimensionSelect}>
+              <option value="poverty">Poverty</option>
+              <option value="healthcare">Health Care</option>
+              <option value="income" default>
+                Income
+              </option>
             </select>
           </form>
         </div>
--- src/components/scatterplot/index.js
+++ src/components/scatterplot/index.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./scatterplot.css";
 import * as d3 from "d3";
 
 const ScatterPlot = (props) => {
-  const svg = d3.select("#plot");
+  //references to DOM nodes
+  const chartRef = useRef(null);
+  //state for width and height to be updated from props
+  const [width, setWidth] = useState(40);
+  const [height, setHeight] = useState(40);
+  const [padding, setPadding] = useState(10);
+  useEffect(() => {
+    if (!chartRef.current) return;
 
-  const circle = svg
-    .append("circle")
-    .attr("fill", "steelblue")
-    .attr("fill-opacity", 0.6)
-    .attr("stroke", "white")
-    .attr("stroke-width", 3);
+    console.log(props);
+    const margin = {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 20,
+    };
 
+    const width = props.width - margin.left - margin.right;
+    const height = props.height - margin.top - margin.bottom;
+    setWidth(width);
+    setHeight(height);
+    setPadding(10);
+    //const svg = d3.select(chartRef.current);
+  }, [props]);
   const xScale = d3
     .scaleLinear()
-    .domain(d3.extent(props.data, props.xDimension));
-  //.range([padding, width - padding]);
+    .domain([d3.min(props.xDimensionData), d3.max(props.xDimensionData)])
+    .range([padding, width - padding]);
   const yScale = d3
     .scaleLinear()
-    .domain(d3.extent(props.data, props.yDimension));
-  //.range([padding, height - padding]);
+    .domain([d3.min(props.yDimensionData), d3.max(props.yDimensionData)])
+    .range([padding, height - padding]);
 
   return (
-    <div className="scatterplot" id="plot">
-      <h3>Scatterplot goes here</h3>
-    </div>
+    <svg ref={chartRef} width={width} height={height}>
+      <g>
+        {props.data.map((d) => (
+          <circle
+            key={d.id}
+            className="circles"
+            r={7}
+            cx={xScale(d[props.xDimensionKey])}
+            cy={yScale(d[props.yDimensionKey])}
+          />
+        ))}
+      </g>
+    </svg>
   );
 };
 
